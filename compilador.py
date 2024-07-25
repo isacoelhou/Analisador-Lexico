9fdce861bd4e50008bc3e93b2e60b8ed69f6816c
isacoelhou <isadora.araujo1@unioeste.br>
2024-07-25 13:38:02 -0300

    .

---
+++ compilador.py
@@ -0,0 +1,22 @@
+def getPalavraReservada(palavra){
+
+}
+
+def getVar(var){
+
+}
+
+def getOpAritmetico(op){
+
+}
+
+def getOpRelacional(op){
+
+}
+
+def getOpLogico(op){
+
+}
+
+token = ""
+token = input()
