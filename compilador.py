59a92c5523f758eb5e1c0b5e94f67bde059bcda1
isacoelhou <isadora.araujo1@unioeste.br>
2024-07-25 19:07:51 -0300

    match

--- compilador.py
+++ compilador.py
@@ -1,22 +1,85 @@
-def getPalavraReservada(palavra){
+def getPalavraReservada(palavra):
+    return
 
-}
+def getVar(var):
+    return
 
-def getVar(var){
+def getOpAritmetico(op):
+    return
 
-}
+def getOpRelacional(op):
+    return
 
-def getOpAritmetico(op){
+def getOpLogico(op):
+    return
 
-}
+def getNum(num):
+    return
 
-def getOpRelacional(op){
+def getFloat(num):
+    return
 
-}
+token = input("Enter a token: ")
 
-def getOpLogico(op){
+match token[0]:
 
-}
+    case _ if token[0].isalpha():
+        getVar(token)
 
-token = ""
-token = input()
+    case _ if token[0].isdigit():
+
+        if token.__contains__('.'):
+            getFloat(token)
+        else:
+            getNum(token)
+
+    case '_':
+        getPalavraReservada(token)
+
+    case '+' | '-' | '*' | '/':
+        getOpAritmetico(token)
+
+    case '&' | '|' :
+        getOpLogico(token)
+
+    case ' ' | '    ':
+        print('espaços')
+    
+    case '(':
+        print('abre parenteses')
+
+    case ')':
+        print('fecha parenteses')
+
+    case '{':
+        print('abre chaves')
+
+    case '}':
+        print('fecha chaves')
+
+    case '[':
+        print('abre colchete')
+
+    case ']':
+        print('fecha colchete')
+
+    case ';':
+        print('ponto e virgula')
+
+    case ',':
+        print('virgula')
+    
+    case '>' | '<':
+        getOpRelacional(token)
+    
+    case ':':
+        if token[1] == ':':
+            getOpRelacional(token)
+        else:
+            print('ERRO: Não é um token')
+        
+
+    
+
+     
+    # case 
