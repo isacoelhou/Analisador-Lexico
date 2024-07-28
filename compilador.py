979ce1e258d2c925c6f4b15900b1c14e6231318b
isacoelhou <isadora.araujo1@unioeste.br>
2024-07-28 10:12:33 -0300

    identifica classe dos tokens

--- compilador.py
+++ compilador.py
@@ -1,7 +1,15 @@
+import re
+
 palavras_reservadas = [
     '_for', '_begin', '_end', '_receba', '_seliga', '_if', '_else', 
     '_int', '_float', '_bool', '_while', '_true', '_false', '_pot'
 ]
+
+operadores_aritmeticos = ['+', '-', '*', '/']
+
+operadores_relacionais = ['::', '>', '<']
+
+operadores_logicos = ['&', '|']
 
 def getPalavraReservada(palavra):
     if palavra in palavras_reservadas:
@@ -11,22 +19,36 @@ def getPalavraReservada(palavra):
         return -1
 
 def getVar(var):
-    return
-
-def getOpAritmetico(op):
-    return
-
-def getOpRelacional(op):
-    return
-
-def getOpLogico(op):
-    return
+    padrao = re.compile(r'^[a-zA-Z0-9]+$') #Padrão suportado pela declaração de variaveis, apenas números e letras
+    if padrao.match(var):
+        return True
+    else:
+        return False
 
 def getNum(num):
-    return
+    padrao = re.compile(r'^[0-9]+$') #Verifica se só tem números na variavel
+    if padrao.match(num):
+        return True
+    else:
+        return False
 
 def getFloat(num):
-    return
+    padrao = re.compile(r'^[0-9.]+$') #Verifica se só tem números e ponto final na variavel
+    if padrao.match(num):
+        if num.count('.') <= 1: #Não pode haver mais de um . na variavel
+            return True
+    else:
+        return False
+
+def getOpAritmetico(op):
+    if op.lenght() > 1:
+        return -1
+    else: 
+        if op in operadores_aritmeticos:
+            posicao = operadores_aritmeticos.index(op)        
+            return posicao
+        else:
+            return -1
 
 def adicionar_par(linha, posicao, tipo):
     tokens.append((linha, posicao, tipo))
@@ -38,14 +60,16 @@ linha = 0
 match token[0]:
 
     case _ if token[0].isalpha():
-        getVar(token)
+        if(getVar(token)):
+            adicionar_par(linha, None, 'Variável')
 
     case _ if token[0].isdigit():
-
         if token.__contains__('.'):
-            getFloat(token)
+            if(getFloat(token)):
+                adicionar_par(linha, None, 'Float')
         else:
-            getNum(token)
+            if(getNum(token)):
+                adicionar_par(linha, None, 'Inteiro')
 
     case '_':
         pos = getPalavraReservada(token)
@@ -53,10 +77,17 @@ match token[0]:
             adicionar_par(linha, pos, 'Palavra Reservada')
 
     case '+' | '-' | '*' | '/':
-        getOpAritmetico(token)
+        pos = getOpAritmetico(token)
+        if(pos != -1):
+            adicionar_par(linha, pos, 'Operador Aritmético')
 
     case '&' | '|' :
-        getOpLogico(token)
+        if token.lenght() == 1:
+            if token == '&':
+                pos = 0
+            else:
+                pos = 1
+            adicionar_par(linha, pos, 'Operador Lógico')
 
     case ' ' | '    ':
         print('espaços')
@@ -85,13 +116,18 @@ match token[0]:
     case ',':
         print('virgula')
     
-    case '>' | '<':
-        getOpRelacional(token)
-    
     case ':':
-        if token[1] == ':':
-            getOpRelacional(token)
+        if token[1] == ':' and token.lengh() == 2:
+            adicionar_par(linha, 0, 'Operador Relacional')
         else:
             print('ERRO: Não é um token')
+
+    case '>':
+        if token.lenght() == 1:
+            adicionar_par(linha, 1, 'Operador Relacional')
+    
+    case '<':
+        if token.lenght() == 1:
+            adicionar_par(linha, 2, 'Operador Relacional')
         
 print(tokens)
